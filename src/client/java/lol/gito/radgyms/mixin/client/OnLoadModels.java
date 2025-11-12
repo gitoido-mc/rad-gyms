ecd4861ab7478d0b1aed31e139851915dd8ac550
Gitoido <warzgibz@gmail.com>
2025-11-12 13:08:13 +0300

    feat+refactor(client): Custom models for gym keys

--- src/client/java/lol/gito/radgyms/mixin/client/OnLoadModels.java
+++ src/client/java/lol/gito/radgyms/mixin/client/OnLoadModels.java
@@ -8,13 +8,13 @@
 
 package lol.gito.radgyms.mixin.client;
 
-import lol.gito.radgyms.client.model.ISpecialItemModel;
+import lol.gito.radgyms.RadGyms;
+import lol.gito.radgyms.RadGymsClient;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.render.model.ModelLoader;
 import net.minecraft.client.render.model.json.JsonUnbakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.ModelIdentifier;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Final;
@@ -24,13 +24,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 @Mixin(ModelLoader.class)
 public abstract class OnLoadModels {
-
     @Shadow
     @Final
     private Map<Identifier, JsonUnbakedModel> jsonUnbakedModels;
@@ -39,21 +38,35 @@ public abstract class OnLoadModels {
     protected abstract void loadItemModel(ModelIdentifier id);
 
     @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", ordinal = 0, shift = At.Shift.AFTER))
-    public void init(BlockColors blockColors, Profiler profiler, Map<Identifier, JsonUnbakedModel> jsonUnbakedModels,
-                     Map<Identifier, List<ModelLoader.SpriteGetter>> blockStates, CallbackInfo ci) {
-        for (Item item : Registries.ITEM) {
-            if (item instanceof ISpecialItemModel loader) {
-                Stream<Identifier> filteredUnbakedModels = this.jsonUnbakedModels.keySet().stream()
-                        .filter(id -> {
-                            String path = id.getPath();
-                            return path.startsWith("models/item/") && path.endsWith(".json");
-                        })
-                        .map(id -> id.withPath(id.getPath().substring(
-                                "models/item/".length(), id.getPath().length() - ".json".length())));
-
-                loader.loadModels(filteredUnbakedModels, this::loadItemModel);
-            }
-        }
+    public void init(
+        BlockColors blockColors,
+        Profiler profiler,
+        Map<Identifier, JsonUnbakedModel> jsonUnbakedModels,
+        Map<Identifier, List<ModelLoader.SpriteGetter>> blockStates,
+        CallbackInfo ci
+    ) {
+        this.jsonUnbakedModels.keySet().stream()
+            .filter(id -> {
+                String path = id.getPath();
+                boolean check = path.startsWith("models/item/gym_key") && path.endsWith(".json");
+                if (check) {
+                    RadGyms.INSTANCE.getLOGGER().info(path);
+                }
+                return check;
+            })
+            .map(id -> id
+                .withPath(
+                    id.getPath()
+                        .substring(
+                            "models/item/".length(),
+                            id.getPath().length() - ".json".length()
+                        )
+                )
+            )
+            .forEach(id -> Arrays.stream(ModelTransformationMode.values())
+                .forEach(item ->
+                    this.loadItemModel(RadGymsClient.INSTANCE.modModelId(id, item.asString()))
+                )
+            );
     }
-
 }
