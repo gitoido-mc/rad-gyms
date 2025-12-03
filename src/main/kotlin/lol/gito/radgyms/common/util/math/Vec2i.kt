e469e05ce8c73312d78acd4902f1aacee96e1c5a
Gitoido Okaski <warzgibz@gmail.com>
2025-12-03 20:03:09 +0300

    fix: Vec2i helper stack overflow fix

--- src/main/kotlin/lol/gito/radgyms/common/util/math/Vec2i.kt
+++ src/main/kotlin/lol/gito/radgyms/common/util/math/Vec2i.kt
@@ -4,54 +4,53 @@ import net.minecraft.util.Mth
 import net.minecraft.world.phys.Vec2
 import kotlin.math.roundToInt
 
-class Vec2i {
-    val x: Int
-    val y: Int
+class Vec2i(val x: Int = 0, val y: Int = 0) {
+    companion object {
+        @JvmField
+        val ZERO: Vec2i = Vec2i(0, 0)
 
-    @JvmField
-    val ZERO: Vec2i = Vec2i(0, 0)
+        @JvmField
+        val ONE: Vec2i = Vec2i(1, 1)
 
-    @JvmField
-    val ONE: Vec2i = Vec2i(1, 1)
+        @JvmField
+        val UNIT_X: Vec2i = Vec2i(1, 0)
 
-    @JvmField
-    val UNIT_X: Vec2i = Vec2i(1, 0)
+        @JvmField
+        val NEG_UNIT_X: Vec2i = Vec2i(-1, 0)
 
-    @JvmField
-    val NEG_UNIT_X: Vec2i = Vec2i(-1, 0)
+        @JvmField
+        val UNIT_Y: Vec2i = Vec2i(0, 1)
 
-    @JvmField
-    val UNIT_Y: Vec2i = Vec2i(0, 1)
+        @JvmField
+        val NEG_UNIT_Y: Vec2i = Vec2i(0, -1)
 
-    @JvmField
-    val NEG_UNIT_Y: Vec2i = Vec2i(0, -1)
+        @JvmField
+        val MAX: Vec2i = Vec2i(Int.MAX_VALUE, Int.MAX_VALUE)
 
-    @JvmField
-    val MAX: Vec2i = Vec2i(Int.MAX_VALUE, Int.MAX_VALUE)
+        @JvmField
+        val MIN: Vec2i = Vec2i(Int.MIN_VALUE, Int.MIN_VALUE)
+    }
 
-    @JvmField
-    val MIN: Vec2i = Vec2i(Int.MIN_VALUE, Int.MIN_VALUE)
-
-    constructor(x: Number, y: Number) {
-        this.x = when (x) {
-            is Double -> x.roundToInt()
-            is Float -> x.roundToInt()
+    constructor(x: Number, y: Number) : this(
+        when (x) {
+            is Int -> x
             is Long -> x.toInt()
             is Short -> x.toInt()
             is Byte -> x.toInt()
-            is Int -> x
-            else -> throw TypeCastException("Cannot convert $x to Int")
-        }
-        this.y = when (y) {
-            is Double -> y.roundToInt()
-            is Float -> y.roundToInt()
+            is Double -> x.roundToInt()
+            is Float -> x.roundToInt()
+            else -> throw TypeCastException("Cannot convert X value $x to Int")
+        },
+        when (y) {
+            is Int -> y
             is Long -> y.toInt()
             is Short -> y.toInt()
             is Byte -> y.toInt()
-            is Int -> y
-            else -> throw TypeCastException("Cannot convert $y to Int")
+            is Double -> y.roundToInt()
+            is Float -> y.roundToInt()
+            else -> throw TypeCastException("Cannot convert Y value $y to Int")
         }
-    }
+    )
 
     fun scale(f: Float): Vec2i {
         return Vec2i(x * f, y * f)
@@ -73,7 +72,7 @@ class Vec2i {
         return x == vec2.x && y == vec2.y
     }
 
-    fun normalized(): Vec2i? {
+    fun normalized(): Vec2i {
         val f = Mth.sqrt((x * x + y * y).toFloat())
         return if (f < 1.0E-4f) ZERO else Vec2i(x / f, y / f)
     }
@@ -83,16 +82,16 @@ class Vec2i {
     }
 
     fun lengthSquared(): Int {
-        return this.x * this.x + this.y * this.y
+        return x * x + y * y
     }
 
     fun distanceToSqr(vec2: Vec2): Float {
-        val f: Float = vec2.x - this.x
-        val g: Float = vec2.y - this.y
+        val f: Float = vec2.x - x
+        val g: Float = vec2.y - y
         return f * f + g * g
     }
 
     fun negated(): Vec2i {
-        return Vec2i(-this.x, -this.y)
+        return Vec2i(-x, -y)
     }
 }
