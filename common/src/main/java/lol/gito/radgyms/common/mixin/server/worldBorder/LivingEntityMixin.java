3b4b308b4cdc51274e6461658c75031d89b30d80
Gitoido <warzgibz@gmail.com>
2026-02-11 23:21:05 +0300

    refactor: mixin update

--- common/src/main/java/lol/gito/radgyms/common/mixin/server/worldBorder/LivingEntityMixin.java
+++ common/src/main/java/lol/gito/radgyms/common/mixin/server/worldBorder/LivingEntityMixin.java
@@ -17,6 +17,7 @@ import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+@SuppressWarnings("resource")
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
     @ModifyReceiver(
