760388031fba0ec21350a040c2b60728b6b48ada
Gitoido <warzgibz@gmail.com>
2026-02-14 05:24:43 +0300

    refactor: re-added kick command

---
+++ common/src/main/kotlin/lol/gito/radgyms/common/command/KickCommand.kt
@@ -0,0 +1,67 @@
+package lol.gito.radgyms.common.command
+
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.exceptions.CommandSyntaxException
+import lol.gito.radgyms.common.RadGyms.modId
+import lol.gito.radgyms.common.api.enumeration.GymLeaveReason
+import lol.gito.radgyms.common.api.event.GymEvents
+import lol.gito.radgyms.common.api.event.GymEvents.GYM_LEAVE
+import lol.gito.radgyms.common.extension.displayClientMessage
+import lol.gito.radgyms.common.registry.RadGymsDimensions.RADGYMS_LEVEL_KEY
+import lol.gito.radgyms.common.world.state.RadGymsState
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.Commands
+import net.minecraft.commands.Commands.argument
+import net.minecraft.commands.arguments.EntityArgument
+import net.minecraft.commands.arguments.selector.EntitySelector
+import net.minecraft.network.chat.Component.translatable
+
+object KickCommand : CommandInterface {
+    override fun register(dispatcher: CommandDispatcher<CommandSourceStack>) {
+        val command = Commands.literal("kick")
+            .requires { it.hasPermission(Commands.LEVEL_GAMEMASTERS) }
+            .then(argument("player", EntityArgument.player()))
+            .executes(::execute)
+
+        dispatcher.register(command)
+    }
+
+    override fun execute(context: CommandContext<CommandSourceStack>): Int {
+        val player = try {
+            context.getArgument("player", EntitySelector::class.java).findSinglePlayer(context.source)
+        } catch (_: CommandSyntaxException) {
+            return -1
+        }
+
+        if (player.level().dimension() != RADGYMS_LEVEL_KEY) {
+            if (context.source.isPlayer) {
+                context.source.sendFailure(
+                    translatable(
+                        modId("message.error.command.kick").toLanguageKey(), context.source.player!!.name
+                    )
+                )
+            }
+            return -1
+        }
+
+        player.displayClientMessage(
+            translatable(modId("message.info.command.op_kick").toLanguageKey())
+        )
+
+        RadGymsState.getGymForPlayer(player)?.let {
+            GYM_LEAVE.emit(
+                GymEvents.GymLeaveEvent(
+                    reason = GymLeaveReason.KICK_COMMAND,
+                    player = player,
+                    gym = it,
+                    completed = false,
+                    usedRope = false
+                )
+            )
+        }
+
+        return Command.SINGLE_SUCCESS
+    }
+}
--- common/src/main/kotlin/lol/gito/radgyms/common/command/RadGymsCommands.kt
+++ common/src/main/kotlin/lol/gito/radgyms/common/command/RadGymsCommands.kt
@@ -11,5 +11,6 @@ object RadGymsCommands {
         registry: CommandBuildContext,
         selection: Commands.CommandSelection
     ) {
+        KickCommand.register(dispatcher)
     }
 }
