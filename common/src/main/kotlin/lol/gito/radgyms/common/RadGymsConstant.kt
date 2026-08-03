ed075ca2cd382ac8eb2f88d92ef3a7099801b6f2
Gitoido <warzgibz@gmail.com>
2026-08-03 04:00:04 +0300

    feat: added cooldown for gym key

--- common/src/main/kotlin/lol/gito/radgyms/common/RadGymsConstant.kt
+++ common/src/main/kotlin/lol/gito/radgyms/common/RadGymsConstant.kt
@@ -5,7 +5,7 @@
  * you can obtain one at https://github.com/gitoido-mc/rad-gyms/blob/main/LICENSE.
  */
 
-@file:JvmName("RadGymsConstant")
+@file:JvmName("RadGymsConstants")
 
 package lol.gito.radgyms.common
 
@@ -22,11 +22,10 @@ const val DEFAULT_GYM_TYPE = "default"
 const val COMMANDS_PREFIX = "rg"
 
 const val TELEPORT_PRELOAD_CHUNKS = 4
-const val WORLDGEN_FEATURE_RANDOM_SHIFT_BITS = 4
 const val GYM_SPACING_IN_DIMENSION = 256
 
 // Cooldown of item use in ticks (20t = 5s)
-const val EXIT_ROPE_COOLDOWN = 100
+const val TELEPORT_COOLDOWN = 100
 
 const val MIN_POKE_LEVEL = 1
 const val MAX_POKE_LEVEL = 100
--- common/src/main/kotlin/lol/gito/radgyms/common/item/ExitRope.kt
+++ common/src/main/kotlin/lol/gito/radgyms/common/item/ExitRope.kt
@@ -8,8 +8,8 @@
 package lol.gito.radgyms.common.item
 
 import com.cobblemon.mod.common.item.CobblemonItem
-import lol.gito.radgyms.common.EXIT_ROPE_COOLDOWN
 import lol.gito.radgyms.common.RadGyms.modId
+import lol.gito.radgyms.common.TELEPORT_COOLDOWN
 import lol.gito.radgyms.common.extension.displayClientMessage
 import lol.gito.radgyms.common.helper.tl
 import lol.gito.radgyms.common.helper.tlr
@@ -37,7 +37,7 @@ class ExitRope : CobblemonItem(Properties()) {
             else -> player.displayClientMessage(tl(modId("${EXIT_ROPE.descriptionId}.failed")))
         }
 
-        player.cooldowns.addCooldown(this, EXIT_ROPE_COOLDOWN)
+        player.cooldowns.addCooldown(this, TELEPORT_COOLDOWN)
         return super.use(level, player, hand)
     }
 
